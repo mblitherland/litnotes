a01938cd33011d8dc0cdab405383d153b89c4537
M Litherland <mlitherland@duck.com>
2024-03-17 16:57:43 -0400

    Brought in some important MUI stuff, made the first components

---
+++ src/components/app.jsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+export default function App() {
+  return (
+    <h2>Welcome to Litnotes</h2>
+  );
+}
--- src/renderer.js
+++ src/renderer.js
@@ -27,6 +27,6 @@
  */
 
 import './index.css';
-import './app.jsx';
+import './index.jsx';
 
 console.log('👋 This message is being logged by "renderer.js", included via webpack');

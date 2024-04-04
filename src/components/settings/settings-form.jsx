bb277b838ea064875c4f0e7bee632ce975177e23
M Litherland <mlitherland@duck.com>
2024-04-04 19:58:53 -0400

    Browse directory is working

--- src/components/settings/settings-form.jsx
+++ src/components/settings/settings-form.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 
-import { FormControl, FormHelperText, InputLabel, MenuItem, Select } from '@mui/material';
+import { Button, FormControl, FormHelperText, InputLabel, MenuItem, Select } from '@mui/material';
+import FolderOpen from '@mui/icons-material/FolderOpen';
 
 const SettingsForm = ({ settings, setSettings }) => {
   const [ themeName, setThemeName ] = React.useState(settings['themeName']);
+
+  const handleBrowseForWorkSpace = async () => {
+    const result = await window.electronAPI.browseDirectory();
+    const dir = result['filePaths'][0];
+    console.log("got directory: "+dir);
+  }
 
   const handleThemeChange = (event) => {
     settings['themeName'] = event.target.value;
@@ -26,7 +33,21 @@ const SettingsForm = ({ settings, setSettings }) => {
           <MenuItem value="light">Light</MenuItem>
         </Select>
         <FormHelperText id="settings-select-theme-helper-text">
-          This change requires a restart.
+          Choose a theme for Litnotes. This change requires a restart.
+        </FormHelperText>
+      </FormControl>
+      <FormControl sx={{ m: 2, minWidth: 120 }}>
+        <Button
+          id="settings-open-workspace"
+          component="label"
+          color="inherit"
+          startIcon={<FolderOpen />}
+          onClick={handleBrowseForWorkSpace}
+        >
+          Select workspace directory
+        </Button>
+        <FormHelperText id="settings-open-workspace-helper-text">
+          Add a workspace.
         </FormHelperText>
       </FormControl>
     </>

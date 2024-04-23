fe7c300d249b2177faaadc9d4cca3dd769516637
M Litherland <mlitherland@duck.com>
2024-04-23 15:38:39 -0400

    Starting to add the tree nodes

--- src/components/workspace-tree.jsx
+++ src/components/workspace-tree.jsx
@@ -7,18 +7,50 @@ import { TreeItem } from '@mui/x-tree-view/TreeItem';
 
 const WorkspaceTree = ({ workspaceTree }) => {
 
+  const nodeIndexes = [];
+
   React.useEffect(() => {
-    console.log("In wt, workspaceTree updated");
-    console.log(workspaceTree);
-    Object.entries(workspaceTree).map(([key, val]) => {
-      console.log("k", key, "v", val);
+    // console.log("In wt, workspaceTree updated");
+    if (!('children' in workspaceTree)) {
+      return;
+    }
+    console.log("tree >>>", workspaceTree);
+    Object.entries(workspaceTree['children']).map(([key, val]) => {
+      // console.log("k", key, "v", val);
     });
   }, [ workspaceTree ]);
+
+  const DirNode = ({ name, subtree }) => {
+    var length = nodeIndexes.push(subtree['path']);
+
+    return (
+      <TreeItem nodeId={(length - 1).toString()} label={name}>
+
+      </TreeItem>
+    );
+  };
+
+  const FileNode = ({ name, node }) => {
+    var length = nodeIndexes.push(node['path']);
+    return <TreeItem nodeId={(length - 1).toString()} label={name} />;
+  }
+
+  const Node = ({ name, node }) => {
+    if (!('type' in node)) {
+      return;
+    }
+    if (node['type'] === 'file') {
+      return <FileNode name={name} node={node} />;
+    }
+    if (node['type'] === 'dir' || node['type'] === 'workspace') {
+      return <DirNode name={name} subtree={node} />;
+    }
+  }
 
   return (
     <>
       <TreeView sx={{}}>
-        <TreeItem nodeId="0" label="top" />
+        <Node name="Workspace" node={workspaceTree} />
       </TreeView>
     </>
   )

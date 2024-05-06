97aba89c800d2fcd3f71c89cd137a7144acb6451
M Litherland <mlitherland@duck.com>
2024-05-06 16:58:33 -0400

    New tabs are appearing as expected

--- src/components/main/editor-panel.jsx
+++ src/components/main/editor-panel.jsx
@@ -3,19 +3,34 @@ import React from 'react';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 
-const EditorPanel = ({ index, tabText, tabSource }) => {
+const EditorPanel = ({ index, tabText, tabSource, tabType, visible }) => {
 
   return (
-    <Box sx={{ padding : 2 }}>
-      { 
-        tabSource && <Typography>This will be an MDX panel containing {tabSource}</Typography>
-      }
-      {
-        tabText && <Typography>{tabText}</Typography>
-      }
-      {/* TODO: this is just while we're developing the tabs */}
-      <Typography variant="caption" sx={{ opacity: '50%' }}>My index is {index}</Typography>
-    </Box>
+    <div
+      hidden={index !== visible}
+    >
+      { index === visible && tabType === ".md" && (
+        <Box sx={{ padding: 2 }}>
+          { 
+            tabSource && <Typography>This will be an MDX panel containing {tabSource}</Typography>
+          }
+          {
+            tabText && <Typography>{tabText}</Typography>
+          }
+          {/* TODO: this is just while we're developing the tabs */}
+        </Box>
+      )}
+      { index === visible && tabType !== ".md" && (
+        <Box sx={{ padding: 2 }}>
+          { tabType === null && (
+            <Typography>{tabText}</Typography>
+          )}
+          { tabType !== null && (
+            <Typography>Cowardly refusing to handle a file that's not apparently Markdown.</Typography>
+          )}
+        </Box>
+      )}
+    </div>
   );
 };
 
